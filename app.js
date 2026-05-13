a17c2db4d493a48493ef6a2f36eb0efc670742f0
Ubuntu <ubuntu@ip-172-31-24-7.ap-northeast-1.compute.internal>
2026-05-13 07:46:19 +0000

    initial docker app

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http")
+
+const name = process.env.NAME || "unknown"
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.end("OK")
+    return
+  }
+
+  res.end(`Hello ${name}`)
+})
+
+server.listen(3000, () => {
+  console.log("server running on port 3000")
+})
