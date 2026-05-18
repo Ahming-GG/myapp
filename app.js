8f9ebdb820db26d0ffd36ee1a506488dce91a15f
Mint <ahming@gogeek.my>
2026-05-18 13:42:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const server = http.createServer((req, res) => {
     return
   }
 
-<<<<<<< HEAD
-  res.end(`Hello ${name}`)
-=======
-  res.end(`Hello Auto`)
->>>>>>> parent of 9b5caea (Update app.js)
-})
+  res.end(`Hello Auto Deploy`)
+
 
 server.listen(3000, () => {
   console.log("server running on port 3000")
